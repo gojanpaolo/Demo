f68252824363546c6915c0182daa21ae55888f57
gojanpaolo <go.janpaolo@gmail.com>
2018-06-02 19:50:06 -0500

    Added IIS Authentication

--- WebApi/GettingStarted/TodoApi/TodoApi/Startup.cs
+++ WebApi/GettingStarted/TodoApi/TodoApi/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.IISIntegration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -26,6 +27,10 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //must explicitly add authentication scheme to access outside of gldd intranet
+            //https://docs.microsoft.com/en-us/aspnet/core/security/authentication/windowsauth?view=aspnetcore-2.1&tabs=aspnetcore2x#iis
+            services.AddAuthentication(IISDefaults.AuthenticationScheme);
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
