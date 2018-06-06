91d1201c6e4e29ad73cb6e12d90154fed5a59650
gojanpaolo <go.janpaolo@gmail.com>
2018-06-06 17:28:48 -0500

    Added ModelValidation

---
+++ ModelValidation/ModelValidation/UseQuantityRangeAttribute.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using UnitsNet;
+using UnitsNet.Units;
+using Xunit;
+
+namespace UseQuantityRangeAttribute
+{
+    //https://github.com/angularsen/UnitsNet/issues/442
+    public class MyObjectTests
+    {
+        [Theory]
+        [InlineData(-1, false)]
+        [InlineData(0, true)]
+        [InlineData(0.5, true)]
+        [InlineData(1, true)]
+        [InlineData(1.1, false)]
+        public void MyLength_Range(double myLength, bool expectedIsValid)
+        {
+            var myObject = new MyObject();
+            myObject.MyLength = new Length(myLength, LengthUnit.Meter);
+
+            var context = new ValidationContext(myObject);
+            var results = new List<ValidationResult>();
+            bool actualIsValid = Validator.TryValidateObject(myObject, context, results, true);
+
+            Assert.Equal(expectedIsValid, actualIsValid);
+        }
+    }
+
+    public class MyObject
+    {
+        [QuantityRange(0, 1, LengthUnit.Meter)]
+        public Length MyLength { get; set; }
+    }
+
+    public class QuantityRangeAttribute : RangeAttribute
+    {
+        private readonly double _minimum;
+        private readonly double _maximum;
+        private readonly int _unitEnumValue;
+        private readonly Func<object, double> _convertToDouble;
+
+        public QuantityRangeAttribute(double minimum, double maximum, LengthUnit unit) : this(minimum, maximum, (int)unit, o => ((Length)o).As(unit)) { }
+
+        public QuantityRangeAttribute(double minimum, double maximum, RatioUnit unit) : this(minimum, maximum, (int)unit, o => ((Ratio)o).As(unit)) { }
+
+        public QuantityRangeAttribute(double minimum, double maximum, AngleUnit unit) : this(minimum, maximum, (int)unit, o => ((Angle)o).As(unit)) { }
+
+        //TODO: Add other Quantity types
+
+        private QuantityRangeAttribute(double minimum, double maximum, int unitEnumValue, Func<object, double> convertToDouble) : base(minimum, maximum)
+        {
+            _minimum = minimum;
+            _maximum = maximum;
+            _unitEnumValue = unitEnumValue;
+            _convertToDouble = convertToDouble;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            double doubleValue = _convertToDouble(value);
+            return base.IsValid(doubleValue, validationContext);
+        }
+    }
+}
---
+++ ModelValidation/ModelValidation/UseSeparateRangeAttribute.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using UnitsNet;
+using UnitsNet.Units;
+using Xunit;
+
+namespace UseSeparateRangeAttribute
+{
+
+    public class MyObjectTests
+    {
+        [Theory]
+        [InlineData(-1, false)]
+        [InlineData(0, true)]
+        [InlineData(0.5, true)]
+        [InlineData(1, true)]
+        [InlineData(1.1, false)]
+        public void MyLength_Range(double myLength, bool expectedIsValid)
+        {
+            var myObject = new MyObject();
+            myObject.MyLength = new Length(myLength, LengthUnit.Meter);
+
+            var context = new ValidationContext(myObject);
+            var results = new List<ValidationResult>();
+            bool actualIsValid = Validator.TryValidateObject(myObject, context, results, true);
+
+            Assert.Equal(expectedIsValid, actualIsValid);
+        }
+    }
+    public class MyObject
+    {
+        [LengthRange(0, 1, LengthUnit.Meter)]
+        public Length MyLength { get; set; }
+    }
+
+    public sealed class LengthRangeAttribute : ValidationAttribute
+    {
+        private readonly Length _minimum;
+        private readonly Length _maximum;
+        private readonly LengthUnit _unit;
+
+        public LengthRangeAttribute(double mininum, double maximum, LengthUnit unit)
+        {
+            _minimum = new Length(mininum, unit);
+            _maximum = new Length(maximum, unit);
+            _unit = unit;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            Length quantity = (Length)value;
+
+            if (quantity < _minimum || quantity > _maximum)
+            {
+                string unitAbbrevation = UnitSystem.GetDefaultAbbreviation(_unit, null);
+                return new ValidationResult($"The field {validationContext.DisplayName} must be between {_minimum.Value} to {_maximum.Value} {unitAbbrevation}.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
