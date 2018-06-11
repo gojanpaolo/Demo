56c81988202293d7589931d05327d7846801244d
gojanpaolo <go.janpaolo@gmail.com>
2018-06-11 08:55:26 -0500

    Added EFCore\GettingStarted

---
+++ EFCore/GettingStarted/QuickOverview/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QuickOverview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var context = new BloggingContext())
+            {
+                context.Database.EnsureCreated();
+            }
+
+            using (var context = new BloggingContext())
+            {
+                var blog = new Blog { Url = "http://sample.com" };
+                context.Blogs.Add(blog);
+                context.SaveChanges();
+            }
+
+            using (var context = new BloggingContext())
+            {
+                var blogs = context.Blogs
+                    .ToList();
+            }
+        }
+    }
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("DataSource=db.sqlite");
+        }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+
+        public string Title { get; set; }
+
+        public int BlogId { get; set; }
+
+        public Blog Blog { get; set; }
+    }
+}
---
+++ EFCore/GettingStarted/QuickOverview/ReadMe.txt
@@ -0,0 +1,3 @@
+This project is a modified version of https://docs.microsoft.com/en-us/ef/core/
+-	Used SQLite
+- Removed some model properties
